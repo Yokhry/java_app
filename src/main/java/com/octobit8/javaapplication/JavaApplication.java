344c50ff819104374003c19abcc697db2c859faf
Yokhry <prathambaliyan012@gmail.com>
2024-02-21 12:15:40 +0530

    initial commit

---
+++ src/main/java/com/octobit8/javaapplication/JavaApplication.java
@@ -0,0 +1,13 @@
+package com.octobit8.javaapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaApplication.class, args);
+	}
+
+}
